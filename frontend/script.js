bc18f2ec4ab9e0c37b13d865fa865f10b55dfa3c
matteus8 <camachomatteus@gmail.com>
2023-12-28 12:37:39 -0700

    created two pods succfully one quiz and one password page.

--- frontend/script.js
+++ frontend/script.js
@@ -1,10 +1,138 @@
-// frontend/script.js
+const questions = [
+  {
+    text: 'What is the capital of France?',
+    options: ['Paris', 'Berlin', 'London'],
+    correctAnswer: 'Paris'
+  },
+  {
+    text: 'How many continents are there?',
+    options: ['5', '6', '7'],
+    correctAnswer: '7'
+  },
+  {
+    text: 'What is the capital of Japan?',
+    options: ['Tokyo', 'Seoul', 'Beijing'],
+    correctAnswer: 'Tokyo'
+  }
+];
 
-// TODO: Implement JavaScript logic for fetching and displaying quiz questions,
-// submitting answers, and displaying quiz results dynamically.
+let currentQuestionIndex = 0;
+let userAnswers = [];
 
-// Example function for submitting answers
-function submitAnswer() {
-    // TODO: Implement logic for submitting answers
-    console.log('Answer submitted!');
+function startQuiz() {
+  console.log('startQuiz function is called.');
+  showQuestion();
 }
+
+function showQuestion() {
+  const questionText = document.getElementById('question-text');
+  const optionsContainer = document.getElementById('options-container');
+  const nextButton = document.getElementById('next-btn');
+
+  const currentQuestion = questions[currentQuestionIndex];
+  questionText.textContent = currentQuestion.text;
+
+  optionsContainer.innerHTML = '';
+  currentQuestion.options.forEach((option, index) => {
+    const button = document.createElement('button');
+    button.textContent = option;
+    button.className = 'option-btn';
+    button.onclick = () => selectOption(index);
+    optionsContainer.appendChild(button);
+  });
+
+  nextButton.style.display = 'none';
+
+  if (currentQuestionIndex === questions.length - 1) {
+    // Last question, show completion message and results
+    nextButton.textContent = 'Finish';
+    nextButton.onclick = finishQuiz;
+  } else {
+    nextButton.textContent = 'Next';
+    nextButton.onclick = nextQuestion;
+  }
+}
+
+function selectOption(index) {
+  const optionsContainer = document.getElementById('options-container');
+  const buttons = optionsContainer.querySelectorAll('.option-btn');
+
+  buttons.forEach((button, i) => {
+    if (i === index) {
+      button.style.backgroundColor = '#2ecc71';
+    } else {
+      button.style.backgroundColor = '#3498db';
+    }
+    // Disable other buttons once one is selected
+    button.disabled = true;
+  });
+
+  const currentQuestion = questions[currentQuestionIndex];
+  userAnswers[currentQuestionIndex] = currentQuestion.options[index];
+
+  const nextButton = document.getElementById('next-btn');
+  nextButton.style.display = 'block';
+}
+
+function nextQuestion() {
+  if (currentQuestionIndex < questions.length - 1) {
+    currentQuestionIndex++;
+    showQuestion();
+  }
+}
+
+function displayResults() {
+  console.log('display function called');
+
+  // Calculate score
+  const score = questions.reduce((acc, question, index) => {
+    if (userAnswers[index] === question.correctAnswer) {
+      return acc + 1;
+    }
+    return acc;
+  }, 0);
+
+  // Display results
+  console.log('Setting display to block for resultContainer');
+  const resultContainer = document.getElementById('result-container');
+  const quizQuestionsContainer = document.getElementById('quiz-questions-container');
+  const quizResults = document.getElementById('quiz-results');
+
+  resultContainer.style.display = 'block';
+  quizQuestionsContainer.style.display = 'none';
+
+  quizResults.textContent = `Your score: ${score}/${questions.length}`;
+}
+
+function finishQuiz() {
+  console.log('Finish Button Clicked');
+  displayResults();
+}
+
+function resetQuiz() {
+  currentQuestionIndex = 0;
+  userAnswers = [];
+  showQuestion();
+
+  const resultContainer = document.getElementById('result-container');
+  const quizQuestionsContainer = document.getElementById('quiz-questions-container');
+  const nextButton = document.getElementById('next-btn');
+
+  // Update the style of the containers
+  resultContainer.style.display = 'none';
+  quizQuestionsContainer.style.display = 'block'; // Show quiz questions container
+
+  // Reset button text and click event
+  nextButton.textContent = 'Next';
+  nextButton.onclick = nextQuestion;
+
+  // Enable all option buttons
+  const optionsContainer = document.getElementById('options-container');
+  const buttons = optionsContainer.querySelectorAll('.option-btn');
+  buttons.forEach(button => {
+    button.disabled = false;
+  });
+}
+
+// Start the quiz when the page loads
+startQuiz();
